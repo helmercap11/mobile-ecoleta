1a00d0cd368eef19b4ac8c02acb7d5f66ad4ead5
helmercap11 <helmercap@gmail.com>
2020-06-06 15:09:09 +0100

    create home, points, detail, routes and add map

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
 
 
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 export default function App() {
 
@@ -23,7 +23,7 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-      <Home />
+      <Routes />
     </>
   );
 }
